e317adf5e8b51194b01517304282181d0975d60c
Eric Normandeau <eric.normandeau.qc@gmail.com>
2017-11-20 14:37:20 -0500

    Add solutions for problems 1 and 2

---
+++ 01_exercises/01_hello_world/01_hello_world_solution_a_simple.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Documentation
+"""Print "Hello World!" when run from the command line
+
+Usage:
+    ./<script.py>
+"""
+
+# Exercise
+"""
+GOAL:
+    Learn how to launch scripts from the terminal (Windows, Mac, Linux).
+
+NOTE:
+    The integration of scripts from multiple languages is always going to be
+    easier in Bash, so Mac and Linux users will have it easier. There are
+    ways to use Bash in Windows. One of them is to install cygwin, a Linux
+    emulator.
+
+    Whenever you see <script.py>, use the actual name of the script instead.
+
+TODO:
+    1) Fill the code below to print "Hello World!"
+    2) Launch the script in one of two ways:
+
+        a) Tell the Python interpreter to execute the script:
+            `python3 <script.py>`
+
+        b) For Mac and Linux:
+
+            i) make script executable
+                `chmod +x <script.py>`
+
+            ii) tell Bash to run it
+                `./<script.py>`
+
+LEARN:
+    In order to run a script, you either tell the Python program to run it or,
+    better, you give instructions to Bash on the first line of the script that
+    tell it what interpreter to use. We use a line beginning with `#!`, also
+    known as *shebang*, followed by the interpreter to use.
+    
+    Instead of giving the specific path to the Python interpreter, like for
+    example `#!/usr/bin/python3`, we let Bash use the Python interpreter that
+    is presently used in the current Bash environment. This is what the part
+    `/usr/bin/env python3` does. This is MUCH BETTER than giving a specific
+    interpreter path because you DO NOT KNOW which Python interpreter the user
+    of your script is going to use. They may have anaconda or miniconda or
+    another Python interpreter installed.
+
+CONCLUSION:
+    You now know how to write simple scripts and launch them in the terminal.
+    All the future exercises will use these tricks.
+"""
+
+# Print "Hello World!"
+
+# Simple solution
+print("Hello World!")
---
+++ 01_exercises/02_user_input/02_user_input_solution_a.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Documentation
+"""Accept user input with sys.argv
+
+Usage:
+    ./<script.py> name age height
+
+Where:
+    name:   is the first name of the user <string>
+    age:    is the age of the user in years <int>
+    height: is the height of the user in meters <float>
+"""
+
+# Exercise
+"""
+GOAL:
+    Learn how to take user inputs from the command line
+
+NOTE:
+    This basic method works well in a lot of cases. A more advanced approach
+    with named parameters will be covered in a future exercise.
+    
+    Whenever you see <script.py>, use the actual name of the script instead.
+    
+TODO:
+    1) Fill the code below to:
+
+        a) Accept three variables from the user and change them in the
+          appropriate type (eg: integer, float...)
+
+        b) Print a sentence using these values
+
+    2) Launch with appropriate values 
+      `./<script.py> John 32 1.75`
+
+    3) Launch without values
+    4) Launch with a wrong number of values (too few and too many)
+    5) Launch with values of the wrong type (eg: string instead of number)
+
+LEARN:
+    Scripts are not very useful if they cannot accept user input. This simple
+    method works well in a lot of cases. Learn to use `sys.argv` to get user
+    inputs as strings and transform them in another type if needed.
+
+CONCLUSION:
+    You now know how to write simple scripts that accept user inputs. This
+    will be useful for most of the following exercises.
+
+WARNING:
+    This script will crash without a useful error message if the user does not
+    give at least three parameters. The next exercice will give us the tools
+    needed to improve this situation.
+"""
+
+# Modules
+import sys
+
+# Parse user input
+name = sys.argv[1]          # user name, no need to transform since already a string
+age = int(sys.argv[2])      # in years, transform input into an integer (with `int`)
+height = float(sys.argv[3]) # in meters, transform input into a float (with `float`)
+
+# Print informations about the user using Python 3.6 string formatting
+print(f"Hello {name}! You are {age} years old and {height} meters tall.")
