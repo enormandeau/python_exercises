2815cd591f829716b4cb8dbfd2e2a8e3c6488947
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-11-17 13:46:31 -0500

    Improve 01_hello_world.py

--- 01_hello_world/01_hello_world_solution_a_simple.py
+++ 01_hello_world/01_hello_world_solution_a_simple.py
@@ -14,9 +14,7 @@ GOAL:
 
 NOTE:
     The integration of scripts from multiple languages is always going to be
-    easier in Bash, so Mac and Linux users will have it easier. There are
-    ways to use Bash in Windows. One of them is to install cygwin, a Linux
-    emulator.
+    easier in Bash, so Mac and Linux users will have it easier.
 
     Whenever you see <script.py>, use the actual name of the script instead.
 
@@ -36,18 +34,18 @@ TODO:
                 `./<script.py>`
 
 LEARN:
-    In order to run a script, you either tell the Python program to run it or,
-    better, you give instructions to Bash on the first line of the script that
-    tell it what interpreter to use. We use a line beginning with `#!`, also
-    known as *shebang*, followed by the interpreter to use.
+    In order to run a script, you either tell the Python program to run it or
+    you give instructions to Bash on the first line of the script that tells it
+    what interpreter to use. We use a line beginning with `#!`, also known as
+    *shebang*, followed by the interpreter to use.
     
     Instead of giving the specific path to the Python interpreter, like for
     example `#!/usr/bin/python3`, we let Bash use the Python interpreter that
     is presently used in the current Bash environment. This is what the part
     `/usr/bin/env python3` does. This is MUCH BETTER than giving a specific
     interpreter path because you DO NOT KNOW which Python interpreter the user
-    of your script is going to use. They may have anaconda or miniconda or
-    another Python interpreter installed.
+    of your script is going to want to use. They may have anaconda or miniconda
+    or another Python interpreter installed.
 
 CONCLUSION:
     You now know how to write simple scripts and launch them in the terminal.
